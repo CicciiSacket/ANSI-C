15d539d7a9c13196130b3aa30be3afd6bb95f18e
Francesco Sacco <cicciisacco@gmail.com>
2021-10-12 17:45:48 +0200

    init unime

---
+++ UNIME/Init/Init/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  Init
+//
+//  Created by Francesco_Utility on 12/10/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("Sembra di tornare al primo hello word.. \n Che tristezza \n");
+    return 0;
+}
