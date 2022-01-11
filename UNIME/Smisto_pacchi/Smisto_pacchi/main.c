46c7e86c494734d22f389854081b9db0ca1e5eeb
Francesco Sacco <cicciisacco@gmail.com>
2022-01-11 13:49:20 +0100

    Fix project

--- UNIME/Smisto_pacchi/Smisto_pacchi/main.c
+++ UNIME/Smisto_pacchi/Smisto_pacchi/main.c
@@ -20,24 +20,9 @@ typedef struct package { //Etichetta del singolo pacco;
 } package;
 
 struct Tape { //Nastro per i pacchi;
-    package Tape;
-    struct column_A *next;
+    package TapePackage;
+    struct Tape *next;
 };
-
-//struct column_B { //Colonna che contiene i pacchi specifici;
-//    package Tape_B[SIZE_TAPE]; //Nastro per i pacchi in uscita dalla fabbrica;
-//    struct column_B *next;
-//};
-//
-//struct column_C { //Colonna che contiene i pacchi specifici;
-//    package Tape_C[SIZE_TAPE]; //Nastro per i pacchi che rimangono ancora in fabbrica;
-//    struct column_C *next;
-//};
-//
-//struct column_D { //Colonna che contiene i pacchi specifici;
-//    package Tape_D[SIZE_TAPE]; //Nastro per i pacchi difettosi e da incenerire;
-//    struct column_D *next;
-//};
 
 int create_fileManager(char pattern[]) { //Crea il file in lettura e scrittura;
     FILE *fp = fopen(pattern, "w+");
@@ -59,42 +44,50 @@ void write_fileManager_IN(FILE *fp,package *package, typePackage typePackage) { 
     fprintf(fp, "IN -> ID: %s\t  Type: %u\n", package->ID, typePackage);
 }
 
+void write_fileManager_OUT(FILE *fp,package *package, typePackage typePackage) { //Scrittura sul file del pacco e della sua tipologia
+    fprintf(fp, "OUT -> ID: %s\t  Type: %u\n", package->ID, typePackage);
+}
 // ---------------------- //
 
-//struct column_A *delete_in_list(struct column_A *list, struct package *package) { //Elimina un nodo dalla lista
-//    struct column_A *prev,*cur;
-//    for (cur = list, prev = NULL; cur != NULL && strcmp(cur->Tape_A.ID, package->ID) != 0; prev = cur, cur = cur->next);
-//    if (cur == NULL) {
-//        return list;
-//    }
-//    if (prev == NULL) {
-//        list = list->next;
-//
-//    }
-//    else
-//        prev->next = cur->next;
-//    free(cur);
-//    return list;
-//}
-
-
-struct column_A *add_to_list_A(struct column_A *list, struct package *package) {
-    struct column_A *new_node;
-    new_node = malloc(sizeof(struct column_A));
+struct Tape *add_to_tape(struct Tape *list, struct package *package) { //Aggiunge il pacco al nastro specifico
+    struct Tape *new_node;
+    new_node = malloc(sizeof(struct Tape));
     if (new_node == NULL) {
         printf("%s","Error");
         exit(EXIT_FAILURE);
     }
-    new_node->Tape_A = *package;
+    new_node->TapePackage = *package;
     new_node->next = list;
     return new_node;
 }
 
-void all_values_list(struct column_A *list) {
-    struct column_A *i;
-    for (i = list; i != NULL; i = i->next) {
-        printf("%s\n", i->Tape_A.ID);
+struct Tape *search_in_list(struct Tape *list, struct package *package) {
+    for (; list != NULL; list = list->next) {
+        if (strcmp(list->TapePackage.ID, package->ID) == 0) {
+            return list;
+        }
     }
+    return NULL;
+}
+
+void all_packages_Tape(struct Tape *list) {
+    struct Tape *i;
+    for (i = list; i != NULL; i = i->next) {
+        printf("%s\n", i->TapePackage.ID);
+    }
+}
+
+int get_in_category(struct Tape *list, struct package category[], struct package *package,char IdPackage[7]) { //Sposto un pacco dal nastro opportuno alla categoria opportuna
+    struct Tape *search = search_in_list(list, package);
+    if (search == NULL) {
+        return 404;
+    }
+}
+
+void all_package_in_category(struct package category[]) { //Visualizza i pacchi nella categoria;
+        for (int i = 0; i > SIZE_TAPE; i++) {
+            printf("%s\n",category[i].ID);
+        }
 }
 
 
@@ -102,30 +95,33 @@ void all_values_list(struct column_A *list) {
 int main(int argc, const char * argv[]) {
 //    FILE *fp = open_fileManager("/Users/Francesco_Utility/Desktop/Programmazione_I/ANSI-C/UNIME/Smisto_pacchi/Smisto_pacchi/Register.txt");
     
+    struct package *packageTest = {"AA000AA","000A",3.59,defective};
 
-    struct column_A *first_A = NULL; //Punto iniziale della colonna A;
-    struct package Tape_A[SIZE_TAPE]; //Nastro con i pacchi in ingresso;
-    
-    struct package *packegeTest = {"AA000A0", "0000A", 3.56, regular};
-    struct package *packegeTest2 = {"AA000A2", "0000A", 3.56, regular};
-    struct package *packegeTest3 = {"AA000A3", "0000A", 3.56, regular};
+//    struct package category_A[SIZE_TAPE]; //Categoria con i pacchi in spedizione; [ Non colonna perchè non sono spediti in ordine di arrivo al magazzino i pacchi :| ]
+//    struct package category_B[SIZE_TAPE]; //Categoriacon i pacchi in attesa di;
+    struct package *category_C[SIZE_TAPE]; //Categoria con i pacchi da incenerire;
 
-    first_A = add_to_list_A(first_A, packegeTest);
-    first_A = add_to_list_A(first_A, packegeTest);
-//    first_A = add_to_list_A(first_A, packegeTest2);
-//    first_A = add_to_list_A(first_A, packegeTest3);
-//    first_A = add_to_list_A(first_A, packegeTest);
-    
+    struct Tape *Tape_A = NULL; //Lista corrispondente al nastro A;
+    struct Tape *Tape_D = NULL; //Lista corrispondente al nastro D;
 
-    all_values_list(first_A);
-    delete_in_list(first_A, packegeTest);
-    all_values_list(first_A);
+    Tape_A = add_to_tape(Tape_A, packageTest); //Aggiungo il pacco sul nastro A;
+//    write_fileManager_IN(fp,packageTest,regular); //Scrivo che è entrato un nuovo pacco;
+    printf("%s\n","I pacchi in ingresso sono:\n");
+    all_packages_Tape(Tape_A); //Visualizzo i pacchi presenti sul nastro A;
+    Tape_D = add_to_tape(Tape_D,packageTest);//Sposto nel nastro dell'inceneritore il pacco difettoso;
+    printf("%s\n","Il pacco selezionato è stato spostato nel nastro D");
+    printf("%s\n","I pacchi nel nastro D sono:\n");
+    all_packages_Tape(Tape_D); //Visualizzo i pacchi sul nastro dell'inceneritore;
 
-//    write_fileManager_IN(fp,packegeTest,regular);
+    printf("\n\n\n");
+    printf("%d\n",get_in_category(Tape_D, category_C, packageTest,"AA000AA"));//sposto il pacco nella categoria da incenerire
+    printf("%s\n","Il pacco selezionato sarà presto incenerito");
+    printf("\n%s\n", "I pacchi da incenerire sono:");
 
+        
 
-    
-    
 
     return 0;
 }
+//All'arrivo del pacco print delle specifiche - porlo nella colonna corrispondente(scrivendo che il pacco è OUT se deve uscire o Fire se deve bruciare) e di conseguenza prima nel nastro corrispondente - registrare l'ingresso del pacco sul file
+// ....... continue
